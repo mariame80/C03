5f115fca766ef96e3aa034922966a70564bccfc9
mariam <abdoukalassimariam@gmail.com>
2025-07-23 12:28:00 +0100

    c

---
+++ largest_of_three.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, grand;
+    scanf("%d %d %d",&a, &b,&c );
+    if(a>b && a>c ){
+        grand = a;
+    }else if( b>a && b>c){
+        grand = b;
+    }else{
+        grand = c;
+    }
+
+        printf("le plus grand nombre est : %d\n", grand);
+    
+
+    return 0;// fin du programme 
+}
